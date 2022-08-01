3d17b7dcbb6f547f5dbbeb2398a6faa0f4f7c50a
Wade-J-Hudgens <71951530+Wade-J-Hudgens@users.noreply.github.com>
2022-08-01 13:24:07 -0700

    Created base component elements

--- src/components/MySwitch/MySwitch.tsx
+++ src/components/MySwitch/MySwitch.tsx
@@ -1,13 +1,14 @@
-import { FC, MouseEvent, ReactNode, useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
 type SwitchColor = "primary" | "secondary" | "default";
 type SwitchSize = "small" | "medium";
 
 interface IMySwitchProps {
-    label?: String,
+    label?: string,
     color?: SwitchColor,
     size?: SwitchSize,
     checked?: Boolean,
+    disabled?: Boolean,
     onChange?: Function
 }
 
@@ -17,12 +18,34 @@ const MySwitch: FC<IMySwitchProps> = ({
     color = "default",
     size = "medium",
     checked = false,
+    disabled=false,
     onChange = ()=>{}
 }) => {
 
+    const DynamicClassNamesBase: string = "AntraUI-Switch-";
+    const DynamicClassNames: string = [
+        DynamicClassNamesBase+label, 
+        DynamicClassNamesBase+size, 
+        DynamicClassNamesBase+checked, 
+        DynamicClassNamesBase+color,
+        DynamicClassNamesBase+disabled
+    ].join(" ");
+
+    const LabelComponent: FC = () => {
+        return label != "" ?
+        <label className={`AntraUI-SwitchComponent-Switch-Label ${DynamicClassNames}`}>{label}</label>
+        :
+        <></>
+    }
+    
+
     return (
-        <div className="AntraUI-Switch-Container">
-            
+        <div className="AntraUI-SwitchComponent-Container" onClick={()=>{onChange(!checked)}}>
+            <div className={`AntraUI-SwitchComponent-Switch-Container ${DynamicClassNames}`}>
+                <div className={`AntraUI-SwitchComponent-Switch-Track ${DynamicClassNames}`}></div>
+                <div className={`AntraUI-SwitchComponent-Switch-Head ${DynamicClassNames}`}></div>
+            </div>
+            <LabelComponent />
         </div>
     )
 }
