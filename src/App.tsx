8c8fbbf43fbfdc7a059720b5d02d015f4d211870
Andrew Lenhart <andrew.code21@gmail.com>
2022-08-02 11:07:38 -0400

    simple-accordion-skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import MyButton from './components/MyButton/MyButton';
-import MyAccordion from './components/MyAccordion/MyAccordion';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import MyButton from "./components/MyButton/MyButton";
+import MyAccordion from "./components/MyAccordion/MyAccordion";
 
-
+const accordionData = [
+  {
+    id: 0,
+    summary: "Basic Accordion #1 Summary",
+    detail: "Basic Accordion Detail.",
+  },
+  {
+    id: 1,
+    summary: "Basic Accordion #2 Summary",
+    detail: "Basic Accordion Detail.",
+  },
+  {
+    id: 2,
+    summary: "Basic Accordion #3 Summary",
+    detail: "Basic Accordion Detail.",
+  },
+  {
+    id: 3,
+    summary: "Basic Accordion #4 Summary",
+    detail: "Basic Accordion Detail.",
+  },
+];
 
 function App() {
   return (
     <>
-      <div style={{display:"flex", gap:"10px"}}>
-        <MyButton onClick={()=>{console.log("clicked")}} color="primary" variant="contained" disabled>SUBMIT</MyButton>
-        <MyButton onClick={()=>{console.log("clicked")}} color="default" variant="outlined" >SUBMIT</MyButton>
-        <MyButton onClick={()=>{console.log("clicked")}} color="secondary" variant="text" >SUBMIT</MyButton>
-        <MyButton onClick={()=>{console.log("clicked")}} color="secondary" variant="text" disabled>SUBMIT</MyButton>
-        <MyButton onClick={()=>{console.log("clicked")}} color="primary" variant="contained" >SUBMIT</MyButton>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <MyButton
+          onClick={() => {
+            console.log("clicked");
+          }}
+          color="primary"
+          variant="contained"
+          disabled
+        >
+          SUBMIT
+        </MyButton>
+        <MyButton
+          onClick={() => {
+            console.log("clicked");
+          }}
+          color="default"
+          variant="outlined"
+        >
+          SUBMIT
+        </MyButton>
+        <MyButton
+          onClick={() => {
+            console.log("clicked");
+          }}
+          color="secondary"
+          variant="text"
+        >
+          SUBMIT
+        </MyButton>
+        <MyButton
+          onClick={() => {
+            console.log("clicked");
+          }}
+          color="secondary"
+          variant="text"
+          disabled
+        >
+          SUBMIT
+        </MyButton>
+        <MyButton
+          onClick={() => {
+            console.log("clicked");
+          }}
+          color="primary"
+          variant="contained"
+        >
+          SUBMIT
+        </MyButton>
       </div>
-      <div style={{display:"flex", gap:"10px"}}>
-        <MyAccordion />
+
+      <div style={{ display: "block", width: 600, marginTop: 20 }}>
+        {accordionData.map((tab) => (
+          <MyAccordion key={tab.id} summary={tab.summary} detail={tab.detail} />
+        ))}
       </div>
     </>
   );
--- src/components/MyAccordion/MyAccordion.tsx
+++ src/components/MyAccordion/MyAccordion.tsx
@@ -1,12 +1,81 @@
-import { FC } from "react";
+import { ChangeEvent, FC, ReactNode, useCallback, useState } from "react";
 
-interface IMyAccordionProps {}
+type AccordianVariant = "simple" | "controlled" | "action";
 
-const MyAccordion: FC<IMyAccordionProps> = () => {
+interface IMyAccordionProps {
+  // user customize summary
+  summary?: string;
+  // user customize detail
+  detail?: string;
+  // user customize bgColor
+  bgColor?: string;
+  // user customize color
+  color?: string;
+  // user customize accordion variant
+  variant?: AccordianVariant;
+  // accordion expanded state
+  isExpanded?: string | boolean;
+  // customize expandIcon
+  defaultExpandIcon?: ReactNode;
+  // disable accordion
+  disabled?: boolean;
+  // onchange event
+  onChange?: (evt: ChangeEvent) => void;
+}
+
+const DefaultExpandIcon: FC = () => {
   return (
-    <>
-      <h1>My Accordian</h1>
-    </>
+    <svg viewBox="0 0 24 24">
+      <path d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+    </svg>
+  );
+};
+
+const MyAccordion: FC<IMyAccordionProps> = ({
+  summary = "Accordion Summary",
+  detail = "This is an Accordion Detail",
+  bgColor = "rgba(255, 255, 255, 1)",
+  color = "rgba(0, 0, 0, 0.87)",
+  variant = "simple",
+  isExpanded = false,
+  defaultExpandIcon = <DefaultExpandIcon />,
+  disabled = false,
+  onChange,
+}) => {
+  const [expandedState, setExpandedState] = useState(isExpanded);
+
+  const handleChange = useCallback(() => {
+    setExpandedState(!expandedState);
+  }, [expandedState]);
+
+  return (
+    <article
+      className="antraUI-Accordion-container"
+      style={{ backgroundColor: bgColor, marginBottom: expandedState ? 16 : 0}}
+    >
+      <div
+        onClick={handleChange}
+        className="antraUI-Accordion-summary-container"
+        style={{ color: color }}
+      >
+        <p className="antraUI-Accordion-summary-typography">{summary}</p>
+
+        <div className="antraUI-Accordion-icon" style={{ fill: color }}>
+          {defaultExpandIcon}
+        </div>
+      </div>
+
+      {expandedState && (
+        <>
+          <div
+            className="antraUI-Accordion-detail-container"
+            style={{ color: color }}
+          >
+            <p className="antraUI-Accordion-detail-typography">{detail}</p>
+          </div>
+        </>
+      )}
+    </article>
   );
 };
 export default MyAccordion;
