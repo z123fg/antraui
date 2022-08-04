5e3101fc268a7344af009e87f42cd430f1e5cb34
Wade-J-Hudgens <71951530+Wade-J-Hudgens@users.noreply.github.com>
2022-08-04 12:37:31 -0700

    Update MySwitch.tsx

--- src/components/MySwitch/MySwitch.tsx
+++ src/components/MySwitch/MySwitch.tsx
@@ -1,77 +1,77 @@
-import { FC, useEffect, MouseEvent, useState, ReactNode } from "react";
+import { FC, MouseEvent, useState, useRef, useEffect } from 'react';
 
-type SwitchColor = "primary" | "secondary" | "default";
+type SwitchColor = "primary" | "secondary" | "warning" | "default";
+
 type SwitchSize = "small" | "medium";
 
 interface IMySwitchProps {
-    label?: string,
+    /**
+    * customize the color of the switch
+    */
     color?: SwitchColor,
+    /**
+    * customize the size of the switch
+    */
     size?: SwitchSize,
-    checked?: Boolean,
-    disabled?: Boolean,
-    onChange?: (event:MouseEvent) => void
+    /**
+    * customize the label of switch
+    */
+    label?: string,
+    /**
+    * used to disable the switch
+    */
+    disabled?: boolean,
+    /**
+    * used to check the button
+    */
+    checked?: boolean
+    /**
+    * event handler for the click event of the switch
+    */
+    onChange?: (event: MouseEvent) => void
 }
 
-let rippleCounter = 0;
 const MySwitch: FC<IMySwitchProps> = ({
-    label = "",
-    color = "default",
+    color = "primary",
     size = "medium",
+    label,
+    disabled = false,
     checked = false,
-    disabled=false,
     onChange
 }) => {
-    const [rippleArr, setRippleArr] = useState<ReactNode[]>([])
 
-    const DynamicClassNamesBase: string = "AntraUI-Switch-";
-    const DynamicClassNames: string = [
-        label!="" ? DynamicClassNamesBase+"label" : DynamicClassNamesBase+"noLabel", 
-        DynamicClassNamesBase+size, 
-        checked ? DynamicClassNamesBase+"isChecked" : DynamicClassNamesBase+"notChecked", 
-        DynamicClassNamesBase+color,
-        disabled ? DynamicClassNamesBase+"isDisabled" : DynamicClassNamesBase+"notDisabled"
-    ].join(" ");
-
-    const LabelComponent: FC = () => {
-        return label != "" ?
-        <label className={`AntraUI-Switch-Label ${DynamicClassNames}`}>{label}</label>
-        :
-        <></>
-    }
+    const [isChecked, setIsChecked] = useState(checked);
     
+    const thumbRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        setIsChecked(checked);
+    }, [checked])
+
     const handleClick = (e: MouseEvent) => {
-        if (!disabled) {
-            const newRipple = (
-                <div
-                    key={rippleCounter++}
-                    className={`AntraUI-Switch-Ripple ${DynamicClassNames}`}
-                    onAnimationEnd={()=>{
-                        setRippleArr(prev=>{
-                            let nextRippleArr = [...prev];
-                            nextRippleArr.shift();
-                            return nextRippleArr;
-                        })
-                    }}
-                >
-                    
-                </div>
-            )
-            setRippleArr(prev=>[...prev, newRipple]);
-            onChange?.(e); 
+        if (disabled) return;
+        setIsChecked(!isChecked);
+        // Add animation after click
+        if (thumbRef && thumbRef.current) {
+            thumbRef.current.style.animation = !isChecked ? `${color}-${size}-effect 0.3s linear` : `Off-${size}-effect 0.3s linear`;
         }
+        onChange?.(e);
     }
+
+
+    const switchContainer: string = ["switch", disabled ? `switch-disabled` : ""].join(" ");
+    const switchStyle: string = isChecked ? `switch-${color}-${size}` : `switch-${size}-off`;
+
     return (
-        <div className={`AntraUI-Switch-Container ${DynamicClassNames}`} onClick={handleClick}>
-            <div className={`AntraUI-Switch ${DynamicClassNames}`}>
-                <div className={`AntraUI-Switch-Track ${DynamicClassNames}`}></div>
-                <div className={`AntraUI-Switch-HeadHover ${DynamicClassNames}`}></div>
-                <div className={`AntraUI-Switch-Head ${DynamicClassNames}`}>
-                    {rippleArr}
-                </div>
+        <div data-testid="MySwitch--container" className={switchContainer} onClick={handleClick}>
+            <div data-testid="MySwitch--switch" className={switchStyle} >
+                <div className='switch-track-wrapper'><div className='switch-track'></div></div>
+                <div className='switch-thumb-wrapper'><div data-testid="switch-thumb" className='switch-thumb' ref={thumbRef}></div></div>
             </div>
-            <LabelComponent />
+            {label}
         </div>
     )
 }
+
 
 export default MySwitch;
