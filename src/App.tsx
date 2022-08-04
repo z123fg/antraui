421d663809e47f003df3ec9333e5fbdb82b93c8e
Lam Tran(Lucas) <ltran96@protonmail.com>
2022-08-04 12:44:44 -0500

    added highlight selected only and fractional rating

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,16 @@ function App() {
       <MyButton onClick={()=>{console.log("clicked")}} color="primary" variant="contained" >SUBMIT</MyButton>
       </div>
       <div style={{display:"flex", flexDirection:"column", border:'1px solid black', width:'auto'}}>
+        <label>Normal Rating</label>
         <MyRating onValueChange={(newValue)=>{console.log(newValue)}} precision={1}/>
+        <label>Disabled</label>
         <MyRating onValueChange={(newValue)=>{console.log(newValue)}} value={2} precision={0.5} disabled/>
+        <label>Read-only (in-progress)</label>
+        <MyRating onValueChange={(newValue)=>{console.log(newValue)}} value={3} precision={0.5} readOnly/>
+        <label>Highlight Selected Only</label>
         <MyRating onValueChange={(newValue)=>{console.log(newValue)}} precision={1} highlightSelectedOnly/>
-        <MyRating onValueChange={(newValue)=>{console.log(newValue)}} precision={1} size="Small"/>
+        <label>Fraction Selection</label>
+        <MyRating onValueChange={(newValue)=>{console.log(newValue)}} precision={0.5} size="Small"/>
       </div>
     </div>
   );
--- src/components/MyRating/MyRating.tsx
+++ src/components/MyRating/MyRating.tsx
@@ -1,4 +1,7 @@
-import { FC, MouseEvent, ReactNode, useEffect, useState } from "react";
+import { FC, MouseEvent, ReactNode, useRef, useState } from "react";
+import StarRoundedIcon from '@mui/icons-material/StarRounded';
+import StarBorderRoundedIcon from '@mui/icons-material/StarBorderRounded';
+import StarHalfRoundedIcon from '@mui/icons-material/StarHalfRounded';
 
 type RatingSize = "Small" | "Medium" | "Large";
 
@@ -36,41 +39,110 @@ const MyRating: FC<IMyRatingProps> = ({
     max = 5,
     disabled = false,
     readOnly = false,
-    precision = 1,
+    precision = 0.5,
     highlightSelectedOnly = false,
-    icon = <span className="star">&#9733;</span>,
-    emptyIcon = <span className="star">&#9733;</span>,
+    icon = <StarRoundedIcon/>,
+    emptyIcon = <StarBorderRoundedIcon color="disabled"/>,
     onValueChange,
     onHoverChange
 }:IMyRatingProps) => {
     const [rating, setRating] = useState<number>(value);
     const [hover, setHover] = useState<number>(value);
+    const [isHover, setIsHover] = useState<boolean>(false);
+    const ratingContainerRef = useRef<HTMLDivElement>(null);
 
-    const handleHover = (rate:number) => {
+    const handleMouseEnter = (e:MouseEvent, rate:number) => {
+      setIsHover(true);
+      if (precision === 1){
         setHover(rate);
         onHoverChange?.(rate);
+      } else {
+        let hoverRating = handlePrecisionRating(e);
+        setHover(rate);
+        onHoverChange?.(rate);
+        if (rate - hoverRating < 0.5){
+          setHover(rate);
+          onHoverChange?.(rate);
+        } else {
+          setHover(hoverRating);
+          onHoverChange?.(hoverRating);
+        }
+      }
+        // handlePrecisionRating(e);
     }
-    const handleClick= (rate:number) => {
+    const handleMouseLeave = (e:MouseEvent, rate:number) => {
+      setIsHover(false);
+      setHover(rate);
+      onHoverChange?.(rate);
+    }
+
+
+    const handleClick= (e:MouseEvent, rate:number) => {
+      if (precision === 1){
         setRating(rate);
         onValueChange?.(rate);
+      } else {
+        let selectedRating = handlePrecisionRating(e);
+        //rate = current star selected 
+        // rating = star user want to show
+        if (rate - selectedRating < 0.5){
+          setRating(rate);
+          onValueChange?.(rate);
+        } else {
+          setRating(selectedRating);
+          onValueChange?.(selectedRating);
+        }
+      }  
+    }
+
+    const handlePrecisionRating = (e:MouseEvent) => {
+      // to get the legnth and width of current container of the rating stars
+      const rect = ratingContainerRef.current?.getBoundingClientRect();
+      const width = rect?.width || 0;
+      const left = rect?.left || 0;
+      let percent = (e.clientX - left)/width;
+      const numberInStar = percent*max;
+      // const nearestNumber = Math.round((numberInStar + precision / 2) / precision) * precision;
+      // console.log("percent: ", percent, "\nNumber of Star: ", numberInStar, "\nNearest number: ", nearestNumber);
+      const preciseResult = Number(numberInStar.toFixed(2))
+      // const result = Number(nearestNumber.toFixed(2));
+      return preciseResult;
     }
     // STILL NEED TO WORK ON RENDER ICON BASED ON THE CONDITION BY CONDITIONALLY RENDER emptyIcon/icon
     return(
-        <div className={`${classes} rate${disabled ? " disabled" : ""}${readOnly ? " read-only": ""}`}>
+        <div 
+        className={`${classes}rate${disabled ? " disabled" : ""}${readOnly ? " read-only": ""}`}
+        ref={ratingContainerRef}
+        >
         {
         [...Array(max)].map((star, index) => {
-          index += 1;
+          index += 1;    
           return (
             <button
               type="button"
               key={index}
               disabled={disabled}
-              className={`icon size${size} ${index <= (hover || rating) ? `iconFilled` : `iconEmpty`} ${highlightSelectedOnly ? index === rating ? "iconFilled" : "iconEmpty" : ""}`}
-              onClick={() => handleClick(index)}
-              onMouseEnter={() => handleHover(index)}
-              onMouseLeave={() => handleHover(rating)}
+              className={`icon size${size}
+              ${disabled ? " disabled": ""}
+              ${highlightSelectedOnly ? index === rating ? "iconFilled" : "iconEmpty" : ""}`}
+              onClick={!readOnly ? (e) => handleClick(e, index) : () => {}}
+              onMouseMove={!readOnly ? (e) => handleMouseEnter(e, index): () => {}}
+              onMouseLeave={!readOnly ?(e) => handleMouseLeave(e, rating): () => {}}
             >
-              {icon}
+              {highlightSelectedOnly
+              ?
+               index === rating ? <StarRoundedIcon color="success"/> : emptyIcon  
+              :  !isHover
+                ? index <= rating 
+                  ? <StarRoundedIcon color="success"/>
+                  :Math.abs(index - rating) < 1  
+                    ? <StarHalfRoundedIcon color="success"/> : emptyIcon
+                : index <= hover 
+                  ? <StarRoundedIcon color="error"/> 
+                  : Math.abs(index - hover) < 1 
+                    ? <StarHalfRoundedIcon color="error"/> 
+                    : emptyIcon
+              }
             </button>
           );
         })
