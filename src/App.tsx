fc46f7722358efd9657150e6bdc3d0d5537e99dd
Lam Tran(Lucas) <ltran96@protonmail.com>
2022-08-04 02:48:06 -0500

    add basic feature for rating components

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,20 @@ import MyRating from './components/MyRating/MyRating';
 
 function App() {
   return (
-    <div style={{display:"flex", gap:"10px"}}>
+    <div style={{display:"flex", flexDirection:"column"}}>
+      <div style={{display: "flex", flexDirection: "row", gap:"10px"}}>
       <MyButton onClick={()=>{console.log("clicked")}} color="primary" variant="contained" disabled>SUBMIT</MyButton>
       <MyButton onClick={()=>{console.log("clicked")}} color="default" variant="outlined" >SUBMIT</MyButton>
       <MyButton onClick={()=>{console.log("clicked")}} color="secondary" variant="text" >SUBMIT</MyButton>
       <MyButton onClick={()=>{console.log("clicked")}} color="secondary" variant="text" disabled>SUBMIT</MyButton>
       <MyButton onClick={()=>{console.log("clicked")}} color="primary" variant="contained" >SUBMIT</MyButton>
-      <MyRating/>
+      </div>
+      <div style={{display:"flex", flexDirection:"column", border:'1px solid black', width:'auto'}}>
+        <MyRating onValueChange={(newValue)=>{console.log(newValue)}} precision={1}/>
+        <MyRating onValueChange={(newValue)=>{console.log(newValue)}} value={2} precision={0.5} disabled/>
+        <MyRating onValueChange={(newValue)=>{console.log(newValue)}} precision={1} highlightSelectedOnly/>
+        <MyRating onValueChange={(newValue)=>{console.log(newValue)}} precision={1} size="Small"/>
+      </div>
     </div>
   );
 }
--- src/components/MyRating/MyRating.tsx
+++ src/components/MyRating/MyRating.tsx
@@ -1,93 +1,80 @@
 import { FC, MouseEvent, ReactNode, useEffect, useState } from "react";
 
-type RatingSize = "small" | "medium" | "large";
+type RatingSize = "Small" | "Medium" | "Large";
 
 interface IMyRatingProps {
-    /**
-     * classes provided by developer
-     */
-    className?: string;
-    /**
-     * default rating when first rendered
-     */
-    defaultValue?: number;
-    /**
-     * customize the size of the rating icon
-     */
+    /** classes provided by developer*/
+    classes?: string;
+    /** default rating when first rendered*/
+    value?: number;
+    /** customize the size of the rating icon*/
     size?: RatingSize;
-    /**
-     * Maximum rating.
-     */
+    /** Maximum rating.*/
     max?: number;
-    /**
-     * used to disable the rating
-     */
+    /** used to disable the rating*/
     disabled?: boolean;
-    /**
-     * used to disable hovering/select effect
-     */ 
-    readonly?: boolean;
-    /**
-     * minimum increment value allowed
-     */ 
+    /** used to disable hovering/select effect*/ 
+    readOnly?: boolean;   
+    /**minimum increment value allowed*/ 
     precision?: number;
-    /**
-     * custom icon specified by developers
-     */
+    /** Only highlight the selected value, the rest set to iconEmpty */ 
+    highlightSelectedOnly?: boolean;    
+    /** custom icon when selected*/
     icon?: ReactNode;
-    /**
-     * event handler fired when a value is clicked
-     */
+    /** custom icon in normal state*/
+    emptyIcon?: ReactNode;
+    /** event handler fired when a value is clicked */
     onValueChange?:(value:number) => void
-    /**
-     * event handler fired when hovered over the stars
-     */
+    /** event handler fired when hovered over the stars */
     onHoverChange?:(value:number) => void
 }
 
 const MyRating: FC<IMyRatingProps> = ({
-    className = "",
-    defaultValue = 2,
-    size = "medium",
+    classes = "",
+    value = 0,
+    size = "Medium",
     max = 5,
     disabled = false,
-    readonly = false,
-    precision = 0.5,
-    icon,
+    readOnly = false,
+    precision = 1,
+    highlightSelectedOnly = false,
+    icon = <span className="star">&#9733;</span>,
+    emptyIcon = <span className="star">&#9733;</span>,
     onValueChange,
     onHoverChange
-}) => {
-    const [rating, setRating] = useState(defaultValue);
-    const [hover, setHover] = useState(0);
+}:IMyRatingProps) => {
+    const [rating, setRating] = useState<number>(value);
+    const [hover, setHover] = useState<number>(value);
 
-    //package hover with developer defined function
     const handleHover = (rate:number) => {
         setHover(rate);
         onHoverChange?.(rate);
     }
-
     const handleClick= (rate:number) => {
         setRating(rate);
         onValueChange?.(rate);
     }
-
+    // STILL NEED TO WORK ON RENDER ICON BASED ON THE CONDITION BY CONDITIONALLY RENDER emptyIcon/icon
     return(
-        <div className="rate">
-        {[...Array(max)].map((star, index) => {
+        <div className={`${classes} rate${disabled ? " disabled" : ""}${readOnly ? " read-only": ""}`}>
+        {
+        [...Array(max)].map((star, index) => {
           index += 1;
           return (
             <button
               type="button"
               key={index}
-              className={index <= (hover || rating) ? "on" : "off"}
+              disabled={disabled}
+              className={`icon size${size} ${index <= (hover || rating) ? `iconFilled` : `iconEmpty`} ${highlightSelectedOnly ? index === rating ? "iconFilled" : "iconEmpty" : ""}`}
               onClick={() => handleClick(index)}
               onMouseEnter={() => handleHover(index)}
               onMouseLeave={() => handleHover(rating)}
             >
-              <span className="star">&#9733;</span>
+              {icon}
             </button>
           );
-        })}
+        })
+        }
       </div>
     )
 }
