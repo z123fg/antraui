8ddee10f1a59a8586289955d22ba84a3ce514609
ting-hu <huxu@uwplatt.edu>
2022-08-07 21:59:56 -0500

    Storybook looks good, not the local host. SCSS needs regular logic

--- src/components/MyDrawer/MyDrawer.tsx
+++ src/components/MyDrawer/MyDrawer.tsx
@@ -6,38 +6,109 @@ import {
   useEffect,
   useState,
 } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 
 type DrawerDirection = "top" | "left" | "right" | "bottom";
 
 interface IMyDrawerProps {
-  open?: boolean;
+  //open?: boolean;
   direction?: DrawerDirection;
   children?: ReactNode;
   onClick?: (event: MouseEvent) => void;
 }
 
 const MyDrawer: FC<IMyDrawerProps> = ({
-  open = false,
-  direction = "bottom",
+  //open = false,
+  direction = "top",
   children,
   onClick,
 }) => {
+  //type ObjectKey = keyof typeof openDrawer;
+  const [openDrawer, setOpendrawer] = useState({
+    top: false,
+    bottom: false,
+    left: false,
+    right: false,
+  });
+  const [currentDir, setCurrendir] = useState(direction);
+  const onCloseBottom = (e: MouseEvent) => {
+    console.log("openDrawer ", openDrawer);
+    setCurrendir(() => "bottom");
+    setOpendrawer((prev) => {
+      return { ...prev, bottom: !openDrawer.bottom };
+    });
+  };
+
+  const onCloseTop = (e: MouseEvent) => {
+    console.log("openDrawer ", openDrawer);
+    setCurrendir(() => "top");
+    setOpendrawer((prev) => {
+      return {
+        ...prev,
+        top: !openDrawer.top,
+      };
+    });
+  };
+
+  const onCloseLeft = (e: MouseEvent) => {
+    console.log("openDrawer ", openDrawer);
+    setCurrendir(() => "left");
+    setOpendrawer((prev) => {
+      return {
+        ...prev,
+        left: !openDrawer.left,
+      };
+    });
+  };
+
+  const onCloseRight = (e: MouseEvent) => {
+    console.log("openDrawer ", openDrawer);
+    setCurrendir(() => "right");
+    setOpendrawer((prev) => {
+      return {
+        ...prev,
+        right: !openDrawer.right,
+      };
+    });
+  };
+
   const constructClassName: () => string = () => {
-    const drawerDir = `drawer-${direction}`;
-    return ["drawer", drawerDir].join(" ");
+    console.log("currentDir ", currentDir);
+    type ObjectKey = keyof typeof openDrawer;
+    const myDir = currentDir as ObjectKey;
+    var openState = openDrawer[myDir];
+
+    return `drawer-${currentDir}-${openState}`;
+  };
+
+  const constructUlClassName: () => string = () => {
+    return `drawer-${currentDir}-container`;
   };
 
   return (
     <div>
-      <div id="btn-group">
-        <button className="button" id="button"></button>
+      <div className="btn-group">
+        <button onClick={onCloseBottom}>
+          <p>B</p>
+          <MenuIcon className="button" id="button" />
+        </button>
+        <button onClick={onCloseTop}>
+          <p>T</p>
+          <MenuIcon className="button" id="button" />
+        </button>
+        <button onClick={onCloseLeft}>
+          <p>L</p>
+          <MenuIcon className="button" id="button" />
+        </button>
+        <button onClick={onCloseRight}>
+          <p>R</p>
+          <MenuIcon className="button" id="button" />
+        </button>
       </div>
 
-      {/* <button className="button" id="button"></button> */}
-
       <nav id="drawer" className={constructClassName()}>
-        <ul id="drawer-container">
-          <li>Item One</li>
+        <ul id="drawer-container" className={constructUlClassName()}>
+          <li>{constructClassName()}</li>
           <li>Item Two</li>
           <li>Item Three</li>
           <li>Item Four</li>
